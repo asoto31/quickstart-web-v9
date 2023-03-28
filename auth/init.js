7699a468575ff29d54d3f07a043ad057f33dabc8
Adrian Soto <sotoperez@google.com>
2023-03-28 13:43:34 -0600

    Messaging in process

--- auth/init.js
+++ auth/init.js
@@ -4,14 +4,14 @@ import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js';
 
 const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBKQSNOdz83RUz9HYbVV3gjdfcvmrpmliI",
-  authDomain: "replicator-37607.firebaseapp.com",
-  databaseURL: "https://replicator-37607.firebaseio.com",
-  projectId: "replicator-37607",
-  storageBucket: "replicator-37607.appspot.com",
-  messagingSenderId: "1082371143398",
-  appId: "1:1082371143398:web:e3b0f970797e9303bd6d27",
-  measurementId: "G-RDVGL3ZN2L"
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
 });
 
 export const auth = getAuth();
