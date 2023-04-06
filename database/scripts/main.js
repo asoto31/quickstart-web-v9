300d7c2be858e8b76f87b71734a9696aa7e7e784
kikemaya <enrique.mg.cd@gmail.com>
2023-04-06 13:54:53 -0600

    valid credentials, replaced

--- database/scripts/main.js
+++ database/scripts/main.js
@@ -27,14 +27,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKQSNOdz83RUz9HYbVV3gjdfcvmrpmliI",
-  authDomain: "replicator-37607.firebaseapp.com",
-  databaseURL: "https://replicator-37607.firebaseio.com",
-  projectId: "replicator-37607",
-  storageBucket: "replicator-37607.appspot.com",
-  messagingSenderId: "1082371143398",
-  appId: "1:1082371143398:web:e3b0f970797e9303bd6d27",
-  measurementId: "G-RDVGL3ZN2L",
+  apiKey: 'api-key',
+  authDomain: 'project-id.firebaseapp.com',
+  databaseURL: 'https://project-id.firebaseio.com',
+  projectId: 'project-id',
+  storageBucket: 'project-id.appspot.com',
+  messagingSenderId: 'sender-id',
+  appId: 'app-id',
+  measurementId: 'G-measurement-id',
 };
 
 const app = initializeApp(firebaseConfig);
